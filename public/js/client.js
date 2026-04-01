91a37fc25af00d908a63f9965ce9a67f083d6b48
lordwillmore <lordwillmore22@gmail.com>
2026-04-01 14:58:30 +0530

    change text

--- public/js/client.js
+++ public/js/client.js
@@ -7,19 +7,11 @@ var BLACK_ROCKET_ICON =
 
 TrelloPowerUp.initialize({
   "card-buttons": function (t, options) {
-    return t.set("member", "shared", "hello", "world").then(function () {
-      return [
-        {
-          icon: BLACK_ROCKET_ICON,
-          text: "Estimate Size",
-          callback: function (t) {
-            return t.popup({
-              title: "Estimation",
-              url: "estimate.html",
-            });
-          },
-        },
-      ];
-    });
+    return [
+      {
+        icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+        text: "Estimate Size",
+      },
+    ];
   },
 });
