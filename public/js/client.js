b271790b9365eb89f39a27d9f842d9988009ad1b
lordwillmore <lordwillmore22@gmail.com>
2026-04-12 15:57:02 +0530

    add abc=def

--- public/js/client.js
+++ public/js/client.js
@@ -22,6 +22,7 @@ TrelloPowerUp.initialize({
   },
   'format-url': function (t, options) {
     // options.url has the url that we are being asked to format
+    let abc = 'def';
     return {
       icon: BLACK_ROCKET_ICON, // don't use a colored icon here
       text: '👉 ' + options.url + ' 👈',
